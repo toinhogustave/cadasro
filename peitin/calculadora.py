205db60e7b358e44f56b9908592277f6dce092a6
toinhogustave <toinhogustave@gmail.com>
2026-06-11 15:41:03 -0300

    esqueleto

---
+++ peitin/calculadora.py
@@ -0,0 +1,10 @@
+opcao=int(input("=========calculadora=========\n1.soma\n2.subtracao\n3.multiplicaçao\n4.divisão\nEscolha uma das opções:"))
+
+if opcao==1:
+    soma(a,b)
+elif opçao==2:
+    subtraçao(a,b)
+elif opcao==3:
+    multiplicaçao(a,b)
+elif opcao==4:
+    divisao(a,b)
